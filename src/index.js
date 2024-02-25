a1301f8593b25f348ad2bb6450c47d0e0491599f
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-02-25 17:12:49 +0100

    complete 6

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@
 // HTML to contain info about one specific pokemon. To get you
 // started, we've created some variables for you to use later on:
 
-const image = document.getElementById("image");
-const name = document.getElementById("name");
-const weight = document.getElementById("weight");
-const height = document.getElementById("height");
-const types = document.getElementById("types");
+const image = document.getElementById('image')
+const nameOfPokemon = document.getElementById('name')
+const weight = document.getElementById('weight')
+const height = document.getElementById('height')
+const types = document.getElementById('types')
 
 // 1) Start with updating the fetchPokemons function so that
 //    it's fetching the pokemons from the pokemon endpoint and
@@ -20,8 +20,21 @@ const types = document.getElementById("types");
 //    HINT --> Don't forget to invoke the function
 
 const fetchPokemons = () => {
-  /*Fetch all pokemons here*/
-};
+  fetch('https://pokeapi.co/api/v2/pokemon/?limit=50')
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data)
+      // console.log(data.results)
+      // console.log(data.results[0].name)
+      data.results.forEach((results) => {
+        console.log(results.name)
+      })
+    })
+
+    .catch((error) => console.log('uncought error:', error))
+}
+fetchPokemons()
+/*Fetch all pokemons here*/
 
 // 2) a) As you can see, we get some metadata as well as
 //    the results of the fetch. Change the console.log so
@@ -46,9 +59,23 @@ const fetchPokemons = () => {
 //    Log the data in the console and see what you find.
 
 const fetchBulbasaurData = () => {
-  /*Fetch singular pokemon here*/
-};
+  fetch('https://pokeapi.co/api/v2/pokemon/1/')
+    .then((response) => response.json())
+    .then((bulbasaur) => {
+      console.log(bulbasaur)
+      image.src = bulbasaur.sprites.front_default
+      nameOfPokemon.innerHTML = `${bulbasaur.name} `
+      weight.innerHTML = `${bulbasaur.weight}`
+      height.innerHTML = `${bulbasaur.height}`
+      const typesNames = bulbasaur.types.map((type) => type.type.name)
+      console.log(typesNames)
+      types.innerHTML = `${typesNames}`
+    })
+    .catch((error) => console.log('uncaught error:', error))
 
+  /*Fetch singular pokemon here*/
+}
+fetchBulbasaurData()
 // 5) After familiarizing with the data, we will use the data
 //    to change our table. We will give you the image as a start.
 //    If you named the data something else than json, you change the
